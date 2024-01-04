983f9b1f333fb55a092c262a15d2dd5cf35423c7
dimi67ch <chalatsoglou-dimitrios@web.de>
2024-01-04 21:20:04 +0100

    kmeans fertig

--- kMeansClustering/dup.py
+++ kMeansClustering/dup.py
@@ -1,9 +1,19 @@
 import csv
 
-with open('iris.csv') as data:
+# kleines Python-Programm, das die Zeilen ausgibt, in der Duplikate im Datensatzes vorhanden sind (dient der Verifizierung)
+
+dups = []
+index_bin = []
+
+with open('./iris.csv') as data:
     lines = data.readlines()
     for actLine in range(0,148):
         for compLine in range(1,149):
             if lines[actLine] == lines[compLine] and actLine != compLine:
-                print(actLine, compLine)
-                print(lines[actLine], lines[compLine])
+                dups.append([actLine, compLine])
+    for dup in dups:
+        if dup[0] < dup[1]:
+            temp = dup[0]
+            dup[0] = dup[1]
+            dup[1] = temp
+    print("DUPS:", dups)
